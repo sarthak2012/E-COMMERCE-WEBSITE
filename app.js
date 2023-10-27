b5f66dca96f8c43042e144321870bee8f9fb1350
Sarthak Srivastava <sarthaksrivastava5765@gmail.com>
2023-10-28 00:51:46 +0530

    website

---
+++ app.js
@@ -0,0 +1,136 @@
+const wrapper = document.querySelector(".sliderwrapper");
+const menuItem = document.querySelectorAll(".menuItem");
+
+const products = [
+  {
+    id: 1,
+    title: "Air Force",
+    price: 300,
+    colors: [
+      {
+        code: "black",
+        img: "./images/air.png",
+      },
+      {
+        code: "darkblue",
+        img: "./images/air2.png",
+      },
+    ],
+  },
+  {
+    id: 2,
+    title: "Jordan",
+    price: 100,
+    colors: [
+      {
+        code: "lightgray",
+        img: "./images/jordan.png",
+      },
+      {
+        code: "green",
+        img: "./images/jordan2.png",
+      },
+    ],
+  },
+  {
+    id: 3,
+    title: "Dunk",
+    price: 150,
+    colors: [
+      {
+        code: "lightgray",
+        img: "./images/blazer.png",
+      },
+      {
+        code: "green",
+        img: "./images/blazer2.png",
+      },
+    ],
+  },
+  {
+    id: 4,
+    title: "Cortez",
+    price: 200,
+    colors: [
+      {
+        code: "black",
+        img: "./images/crater.png",
+      },
+      {
+        code: "lightgray",
+        img: "./images/crater2.png",
+      },
+    ],
+  },
+  {
+    id: 5,
+    title: "Hippie",
+    price: 250,
+    colors: [
+      {
+        code: "gray",
+        img: "./images/hippie.png",
+      },
+      {
+        code: "black",
+        img: "./images/hippie2.png",
+      },
+    ],
+  },
+];
+
+let choosenProduct = products[0];
+
+const currentProductImg = document.querySelector(".productImg");
+const currentProductTitle = document.querySelector(".productTitle");
+const currentProductPrice = document.querySelector(".productPrice");
+const currentProductColors = document.querySelectorAll(".color");
+const currentProductSizes = document.querySelectorAll(".size");
+
+menuItem.forEach((item, index) => {
+    item.addEventListener("click", () => {
+        wrapper.style.transform = `translateX(${-100 * index}vw)`;
+
+        choosenProduct = products[index];
+
+        currentProductTitle.textContent = choosenProduct.title;
+        currentProductPrice.textContent = "$" + choosenProduct.price;
+        currentProductImg.src = choosenProduct.colors[0].img;
+        currentProductColors.forEach((color, index) => {
+         color.style.backgroundColor = choosenProduct.colors[index].code;
+        });
+    });
+});
+
+currentProductColors.forEach((color, index) => {
+    color.addEventListener("click", () => {
+      currentProductImg.src = choosenProduct.colors[index].img;
+    });
+  });
+
+  
+currentProductSizes.forEach((size, index) => {
+  size.addEventListener("click", () => {
+    currentProductSizes.forEach((size) => {
+      size.style.backgroundColor = "white";
+      size.style.color = "black";
+    });
+    size.style.backgroundColor = "black";
+    size.style.color = "white";
+  });
+});
+
+
+const productButton = document.querySelector(".productButton");
+const payment = document.querySelector("payment");
+const close = document.querySelector("close");
+//
+productButton.addEventListener("click", () => {
+  payment.style.display = "flex";
+});
+
+close.addEventListener("click", () => {
+  payment.style.display = "none";
+});
+
+  
